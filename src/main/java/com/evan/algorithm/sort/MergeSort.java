eec47ea6ee3a7a8e8ac6e6dda2e203a8d15a00a9
yuwenyi <yuwenyi@hongtu.tech>
2023-06-07 16:50:06 +0800

    feat:add binary

--- src/main/java/com/evan/algorithm/sort/MergeSort.java
+++ src/main/java/com/evan/algorithm/sort/MergeSort.java
@@ -2,8 +2,16 @@ package com.evan.algorithm.sort;
 
 import com.evan.algorithm.utils.ArrayUtils;
 import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author EvanYu
@@ -21,7 +29,46 @@ import java.util.Arrays;
  * 可以在merge的过程中，每次左边比较较小的数一定会产生右边剩余数量的小和，比如 2，4 一定会产生1个属于4下 2的小和
  *
  */
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
 public class MergeSort {
+    int[] arr;
+
+    @Setup(value = Level.Invocation)
+    public void prepare() {
+        arr = ArrayUtils.GenerateRandomIntArray(20, 100);
+    }
+
+    @Benchmark
+    public void mergeSort() {
+        MergeSort.mergeSort(arr);
+    }
+
+    @Benchmark
+    public void mergeSortRecur() {
+        MergeSort.mergeSortWithRecursion(arr);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(MergeSort.class.getSimpleName())
+                // 预热次数
+                .warmupIterations(1)
+                // 预热时间
+                .warmupTime(TimeValue.valueOf("1s"))
+                // 线程数
+                .threads(1)
+                // fork多少单独的jvm进程进行benchmark
+                .forks(1)
+                // 等所有线程预热完成 类似于CyclicBarrier
+                .syncIterations(true)
+                .resultFormat(ResultFormatType.JSON)
+                .build();
+
+        new Runner(opt).run();
+    }
 
     @Test
     public void testMergeSorting() {
