3a12334c545999160bd30ff8a0f5d71f14462edc
yuwenyi <yuwenyi@hongtu.tech>
2023-07-20 15:31:54 +0800

    feat:add search by backtracking

---
+++ src/main/java/com/evan/algorithm/search/backtracking/NQueen.java
@@ -0,0 +1,118 @@
+package com.evan.algorithm.search.backtracking;
+
+import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.*;
+import org.openjdk.jmh.runner.options.*;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author EvanYu
+ * @date 2023/7/20
+ * @description 按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+ * <p>
+ * 51. N 皇后
+ * n皇后问题 研究的是如何将 n个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给你一个整数 n ，返回所有不同的n皇后问题 的解决方案。
+ * 每一种解法包含一个不同的n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位
+ * 输入：n = 4
+ * 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ */
+@State(Scope.Thread)
+@Slf4j
+public class NQueen {
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        int[] queens = new int[n];
+        Arrays.fill(queens, -1);
+        Set<Integer> columns = new HashSet<>();
+        Set<Integer> diagonals1 = new HashSet<>();
+        Set<Integer> diagonals2 = new HashSet<>();
+        backtrack(solutions, queens, n, 0, columns, diagonals1, diagonals2);
+        return solutions;
+    }
+
+    public void backtrack(List<List<String>> solutions, int[] queens, int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            List<String> board = generateBoard(queens, n);
+            solutions.add(board);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            int d1 = row - i;
+            int d2 = row + i;
+            if (columns.contains(i) || diagonals1.contains(d1) || diagonals2.contains(d2)) {
+                continue;
+            }
+
+            // fill
+            queens[row] = i;
+            columns.add(i);
+            diagonals1.add(d1);
+            diagonals2.add(d2);
+
+            backtrack(solutions, queens, n, row + 1, columns, diagonals1, diagonals2);
+
+            // back
+            queens[row] = -1;
+            columns.remove(i);
+            diagonals1.remove(d1);
+            diagonals2.remove(d2);
+        }
+
+    }
+
+    public List<String> generateBoard(int[] queens, int n) {
+        List<String> board = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            char[] row = new char[n];
+            Arrays.fill(row, '.');
+            row[queens[i]] = 'Q';
+            board.add(new String(row));
+        }
+        return board;
+    }
+
+
+    @Test
+    public void testNQueen() {
+        log.debug(solveNQueens(5).toString());
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+    @Warmup(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
+    @Threads(1)
+    @Fork(1)
+    public void benchmarkNQueen() {
+
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(NQueen.class.getSimpleName())
+                // 预热次数
+                .warmupIterations(1)
+                // 预热时间
+                .warmupTime(TimeValue.valueOf("1s"))
+                // 线程数
+                .threads(1)
+                // fork多少单独的jvm进程进行benchmark
+                .forks(1)
+                // 等所有线程预热完成 类似于CyclicBarrier
+                .syncIterations(true)
+                .resultFormat(ResultFormatType.JSON)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
